f7ceed4e4f018b3dab69457c23ec94715792967e
David Luna <21610290@utgz.edu.mx>
2025-09-24 21:24:38 -0600

    config master

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 define('ADMIN', 'admin');
-define('RUTA_PRINCIPAL', 'http://localhost/casasviamar/');
-// define('RUTA_PRINCIPAL', 'https://www.casasviamar.com/');
+//define('RUTA_PRINCIPAL', 'http://localhost/casasviamar/');
+define('RUTA_PRINCIPAL', 'https://www.casasviamar.com/');
 define('RUTA_ADMIN', RUTA_PRINCIPAL . ADMIN . '/');
 define('HOST', 'localhost');
-define('USER', 'root');
-// define('USER', 'u204448082_root');
-define('PASS', '');
-// define('PASS', 'di1234YA');
-define('DATABASE', 'reservas');
-// define('DATABASE', 'u204448082_reservas');
+// define('USER', 'root');
+define('USER', 'u204448082_root');
+// define('PASS', '');
+define('PASS', 'di1234YA');
+// define('DATABASE', 'reservas');
+define('DATABASE', 'u204448082_reservas');
 define('CHARSET', 'charset=utf8');
 define('TITLE', 'VIA-MAR');
 // CREDENCIALES PAYPAL
@@ -17,4 +17,4 @@ define('MONEDA_PAYPAL', 'USD');
 define('CLIENTE_ID', 'ARbJgmCyDLc0AaAYm8r-QGI3kWvrawyt-jITk-pOhH6sh_bkPHwa0Qr5j7CVWLUVGp7OHP1wFPG08Ko2');
 // CREDENCIALES MERCADO PAGO
 define('PUBLIC_KEY', 'TESTUSER2075515844');
-define('ACCESS_TOKEN','5pv5EilDTs');
+define('ACCESS_TOKEN', '5pv5EilDTs');
